ef69414887aec785c0cdbc112d0d76a82c73bfe4
Just be free <justbefree@126.com>
2021-06-10 14:08:42 +0800

    Update package.json

--- packages/queen/examples/main.ts
+++ packages/queen/examples/main.ts
@@ -2,18 +2,16 @@
 * @Author: Just be free
 * @Date:   2020-09-21 17:37:50
 * @Last Modified by:   Just be free
-* @Last Modified time: 2021-06-07 18:31:39
+* @Last Modified time: 2021-06-10 14:06:28
 * @E-mail: justbefree@126.com
 */
 
-// import { createApp } from "vue";
 import * as Vue from "vue";
 import App from "./App.vue";
 import router from "./routes";
 import Queen from "@";
 import svgs from "./svgs";
-console.log("extend svgs", svgs);
-console.log("Queen ", Queen);
+console.log(`Queen ${Queen.version}`);
 Queen.config({ QIcon: svgs });
 const app = Vue.createApp(App);
 app.use(router);
