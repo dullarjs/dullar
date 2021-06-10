15a4ed06aa9500f97ae730b4b6093e1e7b9262ba
Just be free <justbefree@126.com>
2021-06-10 15:27:18 +0800

    Rename vue-framework to cli

--- packages/cli/lib/greeting.js
+++ packages/cli/lib/greeting.js
@@ -2,7 +2,7 @@
 * @Author: Just be free
 * @Date:   2021-05-13 09:51:51
 * @Last Modified by:   Just be free
-* @Last Modified time: 2021-05-13 13:23:20
+* @Last Modified time: 2021-06-10 15:23:37
 * @E-mail: justbefree@126.com
 */
 const qrcode = require('qrcode-terminal');
@@ -11,7 +11,7 @@ module.exports = {
   hi() {
     console.log("===============================================================");
     console.log("|                                                             |");
-    console.log("|             " + chalk.white.bold("Welcome to use @dullar/vue-framework") + "             |");
+    console.log("|                  " + chalk.white.bold("Welcome to use @dullar/cli") + "                  |");
     console.log("|                                                             |");
     console.log("===============================================================");
     console.log();
--- packages/vue-framework/__tests__/vue-framework.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const vueFramework = require('..');
-
-describe('vue-framework', () => {
-    it('needs tests');
-});
