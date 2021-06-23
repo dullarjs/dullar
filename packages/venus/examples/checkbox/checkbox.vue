6e659b7c096754a0afc2d664205f181984d7eb19
Just be free <justbefree@126.com>
2021-06-23 17:18:10 +0800

    docs: update examples

---
+++ packages/venus/examples/checkbox/checkbox.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="checkbox-view">
+    <div id="container">
+      <p>
+        <span>checked disabled</span>
+        <v-checkbox v-model="checkbox1" :disabled="true"></v-checkbox>
+      </p>
+      <p>
+        <span>unchecked disabled</span>
+        <v-checkbox v-model="checkbox3" :disabled="true"></v-checkbox>
+      </p>
+      <p>
+        <span>{{ checkbox2 }}</span>
+        <v-checkbox v-model="checkbox2"></v-checkbox>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+export default class Checkbox extends Vue {
+  public checkbox1 = true;
+  public checkbox2 = false;
+  public checkbox3 = false;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
---
+++ packages/venus/examples/checkbox/code.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="code">
+    <h3>html</h3>
+    <pre><code class="lang-html">{{ exampleCode }}</code></pre>
+    <h3>js</h3>
+    <pre><code class="lang-js">{{ jsCode }}</code></pre>
+  </div>
+</template>
+<script lang="ts">
+  import { Options, Vue } from 'vue-class-component';
+  const exampleCode =
+  `
+  <q-checkbox v-model="checkbox1" :disabled="true"></q-checkbox>
+  <q-checkbox v-model="checkbox2"></q-checkbox>
+  `;
+  const jsCode =
+  `
+  export default class Flex extends Vue {
+    public checkbox1 = true;
+    public checkbox2 = false;
+  }
+  `;
+  @Options({
+    name: "CheckboxCode"
+  })
+  export default class CheckboxCode extends Vue {
+    public exampleCode = exampleCode;
+    public jsCode = jsCode;
+    mounted() {
+      document.querySelectorAll("pre code").forEach(block => window.Prism.highlightElement(block));
+    }
+  }
+</script>
