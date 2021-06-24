ac1414a38f6b5bd7847712fd8d3e5c94a1e1e761
Just be free <justbefree@126.com>
2021-06-24 17:16:09 +0800

    feat: add vlayout component

---
+++ packages/venus/src/layout/index.ts
@@ -0,0 +1,9 @@
+/*
+* @Author: Just be free
+* @Date:   2020-10-21 15:16:31
+* @Last Modified by:   Just be free
+* @Last Modified time: 2021-06-07 16:29:50
+* @E-mail: justbefree@126.com
+*/
+import Layout from "./layout";
+export default Layout;
