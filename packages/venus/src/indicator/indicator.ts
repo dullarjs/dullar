9382c872e4cf1d0dff6caef1a96762f08308b765
Just be free <justbefree@126.com>
2021-06-24 17:40:55 +0800

    feat: add indicator component

---
+++ packages/venus/src/indicator/indicator.ts
@@ -0,0 +1,62 @@
+/*
+* @Author: Just be free
+* @Date:   2020-10-23 11:32:56
+* @Last Modified by:   Just be free
+* @Last Modified time: 2021-06-24 17:39:35
+* @E-mail: justbefree@126.com
+*/
+import { h, withDirectives, vShow, VNode, nextTick, Transition } from "vue";
+import Queen, { mixins, prop, Options } from "../component/Queen";
+import VSpin from "../spin";
+class Props {
+  text?: string
+  spinType?: string
+  spinColor?: string
+  background?: string
+  size?: string|number
+  lockScreen?: boolean
+  transparent?: boolean
+}
+@Options({
+  name: "VIndicator"
+})
+export default class VIndicator extends mixins(Queen).with(Props) {
+  public static componentName = "VIndicator";
+  public visible = false;
+  setVisible(status: boolean): void {
+    this.visible = status;
+  }
+  render() {
+    return h(Transition, { name: "v-indicator" }, {
+      default: () => [
+        withDirectives(h("div", { class: ["v-indicator"] }, {
+          default: () => [
+            h("div", { class: ["v-indicator-mask", this.transparent ? "transparent" : ""] }, {
+              default: () => []
+            }),
+            h("div", {
+                class: ["v-indicator-wrapper"],
+                style: { padding: this.text ? "20px" : "15px", background: this.background }
+              },
+              {
+                default: () => [
+                  h(VSpin,
+                    {
+                      size: this.size, type: this.spinType, color: this.spinColor,
+                      class: ["v-indicator-spin"]
+                    },
+                    { default: () => [] }
+                  ),
+                  withDirectives(h("span", {
+                    class: ["v-indicator-text"],
+                    innerHTML: this.text
+                  }), [[vShow, this.text]])
+                ]
+              }
+            )
+          ]
+        }), [[vShow, this.visible]])
+      ]
+    });
+  }
+}
