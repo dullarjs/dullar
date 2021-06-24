900c2e5018da73e647dc32e9f949958f981a2592
Just be free <justbefree@126.com>
2021-06-24 18:03:22 +0800

    docs: add sticky examples

---
+++ packages/venus/examples/sticky/sticky.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="sticky-view">
+    <div id="container">
+      <template v-for="item in 200" :key="item">
+        <div v-if="item === 20">
+          <v-sticky offsetTop="100">
+            <span>我固定在某个位置{{ item }}</span>
+          </v-sticky>
+        </div>
+        <div v-else-if="item === 100">
+          <v-sticky offsetTop="0">
+            <span>我固定在某个位置{{ item }}</span>
+          </v-sticky>
+        </div>
+        <div v-else-if="item === 120">
+          <div class="box111" ref="container">
+            <v-sticky class="has-container" :container="container">
+              <span>我固定在某个位置{{ item }}</span>
+            </v-sticky>
+          </div>
+        </div>
+        <div v-else>{{ item }}</div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+// import Queen from "../../src/component/Queen";
+export default class Sticky extends Vue {
+  public container = null;
+  mounted() {
+    this.container = this.$refs.container;
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style: none;
+}
+ul li{
+  margin: 20px auto;
+}
+div.box111 {
+  text-align: right;
+  height: 200px;
+  width: 100%;
+  background: #ccc;
+}
+.has-container {
+  width: 100%;
+  text-align: right;
+}
+</style>
