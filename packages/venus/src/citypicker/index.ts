68edc663dfc4f66232c69e26ba3c70e594f510e0
Just be free <justbefree@126.com>
2021-06-24 18:20:11 +0800

    feat: add citypicker component

---
+++ packages/venus/src/citypicker/index.ts
@@ -0,0 +1,10 @@
+/*
+* @Author: Just be free
+* @Date:   2020-10-22 14:42:13
+* @Last Modified by:   Just be free
+* @Last Modified time: 2021-06-07 16:23:16
+* @E-mail: justbefree@126.com
+*/
+
+import Citypicker from "./citypicker";
+export default Citypicker;
