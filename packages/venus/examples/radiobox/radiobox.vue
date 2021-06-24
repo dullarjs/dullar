9fd2b36594fced42c0c1a0b5cff7389c96978ea8
Just be free <justbefree@126.com>
2021-06-24 16:10:12 +0800

    docs: add examples

---
+++ packages/venus/examples/radiobox/radiobox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="radiobox-view">
+    <div id="container">
+      <span>disabled</span>
+      <v-radiobox v-model="radiobox1" :disabled="true"></v-radiobox>
+      <hr>
+      <span>{{ radiobox2 }}</span>
+      <v-radiobox v-model="radiobox2"></v-radiobox>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+export default class Radiobox extends Vue {
+  public radiobox1 = true;
+  public radiobox2 = false;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
