1957fec84b60e56975763b7403137694c72c467a
Just be free <justbefree@126.com>
2021-06-28 13:55:50 +0800

    feat: add field component

---
+++ packages/venus/src/field/index.ts
@@ -0,0 +1,10 @@
+/*
+* @Author: Just be free
+* @Date:   2020-12-07 14:36:02
+* @Last Modified by:   Just be free
+* @Last Modified time: 2021-06-07 16:26:48
+* @E-mail: justbefree@126.com
+*/
+
+import Field from "./field";
+export default Field;
