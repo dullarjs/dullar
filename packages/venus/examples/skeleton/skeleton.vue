b2d85c719219a0e657c36b3a9efb0001fc784db7
Just be free <justbefree@126.com>
2021-06-28 14:01:55 +0800

    docs: add skeleton examples

---
+++ packages/venus/examples/skeleton/skeleton.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="skeleton-view">
+    <div id="container">
+      <v-skeleton rows="10" :avatar="false"></v-skeleton>
+      <v-skeleton rows="10"></v-skeleton>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+export default class Skeleton extends Vue {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
