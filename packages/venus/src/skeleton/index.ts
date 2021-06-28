35f8a04fff04486ba8a48b22fcdf687a91f81d8b
Just be free <justbefree@126.com>
2021-06-28 14:01:19 +0800

    feat: add skeleton component

---
+++ packages/venus/src/skeleton/index.ts
@@ -0,0 +1,10 @@
+/*
+* @Author: Just be free
+* @Date:   2020-11-04 15:11:16
+* @Last Modified by:   Just be free
+* @Last Modified time: 2021-06-07 16:32:20
+* @E-mail: justbefree@126.com
+*/
+
+import Skeleton from "./skeleton";
+export default Skeleton;
