92c72f309e51f8428f5cc7ee0001c2c294dd3256
Just be free <justbefree@126.com>
2021-07-07 16:16:54 +0800

    perf: improve animation performance

--- packages/queen/src/animation-list-item/animation-list-item.ts
+++ packages/queen/src/animation-list-item/animation-list-item.ts
@@ -2,52 +2,23 @@
 * @Author: Just be free
 * @Date:   2020-11-19 17:56:43
 * @Last Modified by:   Just be free
-* @Last Modified time: 2021-06-11 15:08:06
+* @Last Modified time: 2021-07-07 16:03:46
 * @E-mail: justbefree@126.com
 */
 import Queen, { mixins, prop, Options } from "../component/Queen";
-import { h, Transition, withDirectives, vShow, VNode } from "vue";
+import { h } from "vue";
 
 class Props {
   height?: string|number
 }
 @Options({
   name: "QAnimationListItem",
-  inject: ["parentList"],
-  computed: {
-    animation() {
-      return this.parentList.animation;
-    }
-  }
 })
 export default class QAnimationListItem extends mixins(Queen).with(Props) {
   public static componentName = "QAnimationListItem";
-  public show = false;
-  private entered = false;
-  handleAfterEnter(): void {
-    this.entered = true;
-  }
-  mounted() {
-    if (this.animation) {
-      const timer = setTimeout(() => {
-        this.show = true;
-        clearTimeout(timer);
-      }, 1);
-    }
-  }
-  beforeUnmount() {
-    this.show = false;
-  }
   render() {
-    return h(Transition, {
-      name: this.animation ? "q-slide-in" : "",
-      onAfterEnter: this.handleAfterEnter
-    }, {
-      default: () => [
-        withDirectives(h("div", { class: ["q-animation-list-item", this.entered ? "no-transform" : ""], style: { height: `${this.height}px` } }, {
-          default: () => this.getSlots()
-        }), [[vShow, (this.show || !this.animation)]])
-      ]
+    return h("div", { class: ["q-animation-list-item"], style: { height: `${this.height}px` } }, {
+      default: () => [this.getSlots()]
     });
   }
 }
--- packages/queen/src/animation-list/animation-list.ts
+++ packages/queen/src/animation-list/animation-list.ts
@@ -2,72 +2,55 @@
 * @Author: Just be free
 * @Date:   2020-11-19 17:53:44
 * @Last Modified by:   Just be free
-* @Last Modified time: 2021-06-11 15:08:04
+* @Last Modified time: 2021-07-07 16:03:30
 * @E-mail: justbefree@126.com
 */
 import Queen, { mixins, prop, Options } from "../component/Queen";
-import { h, VNode } from "vue";
+import { h, TransitionGroup } from "vue";
 import QFlex from "../flex";
 import QFlexItem from "../flex-item";
-// const VALID_CHILD_COMPONENT = "animation-list-item";
-
+export type animationTypes = "list" | "paragraph";
 class Props {
-  dataList = prop<Array<any>>({ default: () => [] })
-  animation = prop<boolean>({ default: true })
+  animationType = prop<animationTypes>({ default: "list" })
+  duration = prop<number>({ default: 600 })
 }
 @Options({
   name: "QAnimationList",
-  provide() {
-    return {
-      parentList: this
-    }
-  },
-  watch: {
-    // dataList: function (newValue, oldValue) {
-    dataList: function () {
-      this.$nextTick(() => {
-        this.init();
-      });
-    }
-  }
 })
 export default class QAnimationList extends mixins(Queen).with(Props) {
   public static componentName = "QAnimationList";
-  public stackList: VNode[] = [];
-  infinite(slots: VNode[]) {
-    if (slots.length > 0) {
-      const slot = slots.shift();
-      this.stackList.push(slot as VNode);
-      const timer = setTimeout(() => {
-        this.infinite(slots);
-        clearTimeout(timer);
-      }, 50);
-    }
+  handleBeforeEnter(el: HTMLElement) {
+    el.style.opacity = "0";
   }
-  init() {
-    this.stackList = [] as VNode[];
-    const slots = this.getSlots() as any;
-    const children = slots && slots[0] && slots[0].children;
-    if (this.animation) {
-      this.infinite(children as VNode[]);
-    } else {
-      this.stackList = slots;
-    }
-  }
-  mounted() {
-    this.init();
+  handleEnter(el: HTMLElement, done: () => void) {
+    const delay = Number(el.getAttribute("dataindex")) * 5 + 100;
+    setTimeout(() => {
+      el.setAttribute("style", `
+        opacity: 1;
+        transition: opacity ${this.duration}ms;
+        animation: ${this.animationType}-one-by-one ${this.duration}ms infinite;
+        animation-iteration-count: 1;
+      `);
+      done();
+    }, delay);
   }
   render() {
+    const slots = this.getSlots() as any;
+    const children = slots && slots[0] && slots[0].children;
     return h("div", { class: ["q-animation-list"] }, {
       default: () => [
-        h(QFlex,
-          { flexDirection: "column" },
-          {
-            default: () => Array.apply(null, this.stackList).map((item: any, key: number, arr: any[]) => {
-              return h(QFlexItem, { key }, { default: () => [item] });
-            })
-          }
-        )
+        h(QFlex, { flexDirection: "column" }, {
+          default: () => [
+            h(TransitionGroup,
+              { css: false, onEnter: (e: any, fun: any) => this.handleEnter(e, fun), onBeforeEnter: (e: any) => this.handleBeforeEnter(e) },
+              {
+                default: () => Array.apply(null, children as any).map((item: any, key: number, arr: any[]) => {
+                  return h(QFlexItem, { key, dataindex: key }, { default: () => [item] });
+                })
+              }
+            )
+          ]
+        })
       ]
     });
   }
