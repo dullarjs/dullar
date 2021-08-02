42dcf42ca13f7f8fcccffc478ccfc148814e2e77
ygl11223344 <yglgzyx@126.com>
2021-08-02 18:29:43 +0800

    feat: 分类选择器组件提交（category-select） (#1)
    * 分类选择器组件上传
    
    * feat:category-select-商城分类列表选择组件提交
    
    Co-authored-by: yegl <yegl@yuanian.com>

---
+++ packages/mercury/src/category-select/style/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: yegl
+ * @Date: 2021-08-02 09:17:29
+ * @Last Modified by: yegl
+ * @Last Modified time: 2021-08-02 18:14:03
+ * @E-mail: yglgzyx@126.com
+ */
+import "./index.less";
