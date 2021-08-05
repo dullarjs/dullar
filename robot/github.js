6eb013363fbec2c746dc5219952cf7b40ba9e6b0
Just be free <justbefree@126.com>
2021-08-05 11:24:48 +0800

    refactor: update robot notice

--- robot/github.js
+++ robot/github.js
@@ -2,11 +2,11 @@
 * @Author: Just be free
 * @Date:   2021-08-03 09:56:19
 * @Last Modified by:   Just be free
-* @Last Modified time: 2021-08-05 10:49:19
+* @Last Modified time: 2021-08-05 11:24:06
 * @E-mail: justbefree@126.com
 */
 const axios = require("axios");
-const title = `## <font color="info">【@dullar github page publish notice</font>\n`;
+const title = `## <font color="info">【@dullar github page publish notice】</font>\n`;
 const link = "> UI component examples have been updated [Check now](https://dullar.xyz/)";
 const send = (content = "") => {
   axios.post("https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=eff33902-7403-4176-9887-7e99dc52f7dd", {
