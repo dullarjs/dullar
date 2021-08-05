ad3a1b73729ab90a3767d65728bbeacf6b1cf29d
Just be free <justbefree@126.com>
2021-08-05 11:13:11 +0800

    refactor: update github actions

--- robot/github.js
+++ robot/github.js
@@ -2,13 +2,12 @@
 * @Author: Just be free
 * @Date:   2021-08-03 09:56:19
 * @Last Modified by:   Just be free
-* @Last Modified time: 2021-08-03 14:45:59
+* @Last Modified time: 2021-08-05 10:49:19
 * @E-mail: justbefree@126.com
 */
 const axios = require("axios");
-const pkg = require("../lerna.json");
-const title = `## <font color="info">【dullar编译完成】</font>\n`;
-const link = "[查看官网](https://dullar.xyz/)";
+const title = `## <font color="info">【@dullar github page publish notice</font>\n`;
+const link = "> UI component examples have been updated [Check now](https://dullar.xyz/)";
 const send = (content = "") => {
   axios.post("https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=eff33902-7403-4176-9887-7e99dc52f7dd", {
     "msgtype": "markdown",
---
+++ robot/ynpm.js
@@ -0,0 +1,27 @@
+/*
+* @Author: Just be free
+* @Date:   2021-08-05 10:33:28
+* @Last Modified by:   Just be free
+* @Last Modified time: 2021-08-05 10:43:25
+* @E-mail: justbefree@126.com
+*/
+const axios = require("axios");
+const pkg = require("../lerna.json");
+const title = `## <font color="info">【@dullar npm package publish notice】</font>\n`;
+const link = `> A new version of the package @dullar/* (${pkg.version}) has been published, [Check now](https://www.npmjs.com/settings/dullar/packages)`;
+const send = (content = "") => {
+  axios.post("https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=eff33902-7403-4176-9887-7e99dc52f7dd", {
+    "msgtype": "markdown",
+    "markdown": {
+      "content": content
+    }
+  })
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error)
+  });
+}
+
+send(`${title} ${link}`);
