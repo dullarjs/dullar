4b694d5215e0cdde47da7886b08d5d4146995a0b
Just be free <justbefree@126.com>
2021-08-12 16:29:44 +0800

    feat: add elastic-search component

---
+++ packages/mercury/src/elastic-search/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: Just be free
+ * @Date:   2021-08-11 13:15:12
+ * @Last Modified by:   Just be free
+ * @Last Modified time: 2021-08-11 13:45:31
+ * @E-mail: justbefree@126.com
+ */
+import { install } from "../modules/component";
+import YnElasticSearch from "./elastic-search";
+
+export default install(YnElasticSearch);
