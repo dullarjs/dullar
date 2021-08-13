88a6636436644215c2675777226358f76dbae483
Just be free <justbefree@126.com>
2021-08-13 12:01:36 +0800

    feat: add category-mobile component

---
+++ packages/mercury/src/category-mobile/style/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: Just be free
+ * @Date:   2021-08-12 18:14:43
+ * @Last Modified by:   Just be free
+ * @Last Modified time: 2021-08-12 18:14:49
+ * @E-mail: justbefree@126.com
+ */
+import "./index.less";
