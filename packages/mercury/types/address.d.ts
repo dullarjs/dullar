e92b84e16a445c3bf65db4a0ae29e23a88fc0f48
Just be free <justbefree@126.com>
2021-08-16 16:10:41 +0800

    feat: add .d.ts fils

---
+++ packages/mercury/types/address.d.ts
@@ -0,0 +1,9 @@
+/*
+* @Author: Just be free
+* @Date:   2021-08-16 16:09:01
+* @Last Modified by:   Just be free
+* @Last Modified time: 2021-08-16 16:09:38
+* @E-mail: justbefree@126.com
+*/
+import { YnComponents } from "./component";
+export class YnAddress extends YnComponents {}
