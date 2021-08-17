c27c2237f86a7ae42128f363de80510f88462974
Just be free <justbefree@126.com>
2021-08-17 18:08:00 +0800

    feat: new support pull refresh next or previous page

--- packages/mercury/src/category-mobile/category-mobile.js
+++ packages/mercury/src/category-mobile/category-mobile.js
@@ -2,17 +2,18 @@
  * @Author: Just be free
  * @Date:   2021-08-12 18:14:23
  * @Last Modified by:   Just be free
- * @Last Modified time: 2021-08-13 11:59:09
+ * @Last Modified time: 2021-08-17 18:05:03
  * @E-mail: justbefree@126.com
  */
 import { defineComponent, genComponentName } from "../modules/component";
 import Flex from "../flex";
 import FlexItem from "../flex-item";
 import Spin from "../spin";
+import PullRefresh from "../pull-refresh";
 const CAT_CACHE = {};
 export default defineComponent({
   name: "CategoryMobile",
-  components: { Flex, FlexItem, Spin },
+  components: { Flex, FlexItem, Spin, PullRefresh },
   props: {
     categories: {
       type: Array,
@@ -44,10 +45,13 @@ export default defineComponent({
   },
   methods: {
     handleListClick(e) {
+      // console.log("list click", e);
       const { key, cat } = e;
       this.currentTab = key;
       if (CAT_CACHE[cat.cat_id]) {
         this.categoryList = CAT_CACHE[cat.cat_id];
+        // scroll top
+        this.$refs.pullRefresh.setScrollTop(0);
         return;
       }
       this.requestCategory({ parentId: cat.cat_id });
@@ -69,6 +73,22 @@ export default defineComponent({
     },
     handleItemClick(e) {
       this.$emit("itemClick", { ...e });
+    },
+    handlePull(e) {
+      const { direction } = e;
+      const length = this.categories.length;
+      let currentTab = this.currentTab;
+      if (direction === "top") {
+        if (currentTab > 0) {
+          currentTab -= 1;
+        }
+      } else if (direction === "bottom") {
+        if (currentTab < length) {
+          currentTab += 1;
+        }
+      }
+      const cat = this.categories[currentTab];
+      this.handleListClick({ cat, key: currentTab });
     },
   },
   created() {
@@ -106,48 +126,62 @@ export default defineComponent({
                   },
                   []
                 )
-              : Array.apply(null, this.categoryList).map((cat) => {
-                  return [
-                    h("h4", { key: `title-${cat.id}` }, cat.name),
-                    h(
-                      genComponentName("flex"),
-                      { key: `content-${cat.id}`, props: { flexWrap: "wrap" } },
-                      [
-                        ...Array.apply(null, cat.cat_id).map((subCat) => {
-                          return h(
-                            genComponentName("flex-item"),
-                            { key: subCat.id, class: ["goods-item"] },
-                            [
-                              h(
-                                "a",
-                                {
-                                  on: {
-                                    click: this.handleItemClick.bind(
-                                      this,
-                                      subCat
-                                    ),
-                                  },
-                                  class: ["hypelink"],
-                                  attrs: { href: "javascript:;" },
-                                },
+              : h(
+                  genComponentName("pull-refresh"),
+                  {
+                    ref: "pullRefresh",
+                    on: {
+                      pullRefresh: this.handlePull,
+                    },
+                  },
+                  [
+                    Array.apply(null, this.categoryList).map((cat) => {
+                      return [
+                        h("h4", { key: `title-${cat.id}` }, cat.name),
+                        h(
+                          genComponentName("flex"),
+                          {
+                            key: `content-${cat.id}`,
+                            props: { flexWrap: "wrap" },
+                          },
+                          [
+                            ...Array.apply(null, cat.cat_id).map((subCat) => {
+                              return h(
+                                genComponentName("flex-item"),
+                                { key: subCat.id, class: ["goods-item"] },
                                 [
                                   h(
-                                    "img",
-                                    { attrs: { src: subCat.cat_img } },
-                                    []
+                                    "a",
+                                    {
+                                      on: {
+                                        click: this.handleItemClick.bind(
+                                          this,
+                                          subCat
+                                        ),
+                                      },
+                                      class: ["hypelink"],
+                                      attrs: { href: "javascript:;" },
+                                    },
+                                    [
+                                      h(
+                                        "img",
+                                        { attrs: { src: subCat.cat_img } },
+                                        []
+                                      ),
+                                      h("span", { class: ["goods-name"] }, [
+                                        subCat.name,
+                                      ]),
+                                    ]
                                   ),
-                                  h("span", { class: ["goods-name"] }, [
-                                    subCat.name,
-                                  ]),
                                 ]
-                              ),
-                            ]
-                          );
-                        }),
-                      ]
-                    ),
-                  ];
-                }),
+                              );
+                            }),
+                          ]
+                        ),
+                      ];
+                    }),
+                  ]
+                ),
           ]),
         ]),
       ]),
