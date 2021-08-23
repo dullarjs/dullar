860e30a284559b65129a211bb4c39dd28f460d59
liuzhenxiaoqqq <15225978702@163.com>
2021-08-23 11:16:21 +0800

    V.0.1.44 (#5)
    * 添加modal，badge， tooltip组件
    
    * feature： 添加modal，badge， tooltip组件
    
    * feature： 添加modal， badge， tooltip组件
    
    Co-authored-by: liuzhx <liuzhx@yuanian.com>

---
+++ packages/mercury/examples/tooltip/tooltip.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="shipping-tooltip-box">
+    <h2>yn-tooltip</h2>
+    <div class="tooltip-box-main">
+      <yn-tooltip
+        trigger="hover"
+        placement="top"
+        content="top我是tooltip撒旦发生口角是否数据库恢复瑟夫数据返回上课时间发货时间看"
+      >
+        <yn-button class="mt30" type="text">top</yn-button>
+      </yn-tooltip>
+      <div>
+        <yn-tooltip
+          trigger="hover"
+          placement="left"
+          content="left我是tooltip撒旦发生口角是否数据库恢复瑟夫数据返回上课时间发货时间看"
+        >
+          <yn-button class="mt30" type="text">left </yn-button>
+        </yn-tooltip>
+      </div>
+      <yn-tooltip
+        trigger="hover"
+        placement="right"
+        effect="light"
+        content="right我是撒旦发生撒旦发生口角是否数据库恢复瑟夫数据返回上课时间发货时间看"
+      >
+        <yn-button class="mt30" type="text">right</yn-button>
+      </yn-tooltip>
+      <yn-tooltip
+        trigger="hover"
+        placement="bottom"
+        content="bottom我是tooltip撒旦发生口角是否数据库恢复瑟夫数据返回上课时间发货时间看"
+      >
+        <yn-button class="mt30" type="text">bottom</yn-button>
+      </yn-tooltip>
+      <div class="bottom">
+        <yn-tooltip content="我是tooltip" trigger="click">
+          <yn-button class="mt30" type="text">click</yn-button>
+        </yn-tooltip>
+        <yn-tooltip trigger="focus" effect="light">
+          <yn-button class="mt30" type="text">focus</yn-button>
+          <div slot="content">我是内容分发的focus。</div>
+        </yn-tooltip>
+        <yn-tooltip placement="right" effect="light" trigger="hover">
+          <yn-button class="mt30" type="text">hover 插槽</yn-button>
+          <div slot="content">
+            <p>我是tooltip我是tooltip</p>
+            <p>我是tooltip我是tooltip</p>
+            <p>我是tooltip我是tooltip</p>
+          </div>
+        </yn-tooltip>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "YnTooltipPage",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+<style type="text/css">
+.tooltip-box-main {
+  width: 100px;
+  text-align: center;
+  margin-left: 500px;
+  margin-top: 300px;
+  transform: translate(-50%, -50%);
+}
+.middle {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 16px 0;
+}
+.mt30 {
+  margin-top: 30px;
+}
+</style>
---
+++ packages/mercury/src/badge/badge.js
@@ -0,0 +1,75 @@
+/*
+ * @Author: Just be free
+ * @Date:   2021-08-13 16:53:33
+ * @Last Modified by:   Just be free
+ * @Last Modified time: 2021-08-16 18:15:18
+ * @E-mail: justbefree@126.com
+ */
+import { defineComponent } from "../modules/component";
+import { slotsMixins } from "../mixins/slots";
+export default defineComponent({
+  name: "badge",
+  mixins: [slotsMixins],
+  components: {},
+  props: {
+    color: {
+      type: String,
+      default: "#f5222d",
+    },
+    count: {
+      type: Number,
+      default: "",
+    },
+    dot: {
+      type: Boolean,
+      default: false,
+    },
+    showZero: {
+      type: Boolean,
+      default: false,
+    },
+    overflowCount: {
+      type: Number,
+      default: 99,
+    },
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    content() {
+      const { overflowCount, count } = this;
+      return count > overflowCount ? `${overflowCount}+` : count;
+    },
+    showCount() {
+      const { showZero, count } = this;
+      return showZero ? true : count > 0 ? true : false;
+    },
+  },
+  methods: {},
+  render(h) {
+    const { color, dot, count, content, slots, showCount } = this;
+    return h(
+      "span",
+      {
+        class: ["yn-badge"],
+      },
+      [
+        showCount
+          ? h(
+              "sub",
+              {
+                style: {
+                  background: color,
+                  padding: dot || count < 10 ? "0" : "0 4px",
+                },
+                class: ["position", dot ? "yn-badge-dot" : "yn-badge-count"],
+              },
+              dot ? "" : content
+            )
+          : null,
+        h("span", {}, slots()),
+      ]
+    );
+  },
+});
