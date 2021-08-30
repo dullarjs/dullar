9ba954fdfc05946b6c0c233a7d7c0895180421c5
Just be free <justbefree@126.com>
2021-08-30 14:44:35 +0800

    docs: update tree examples

--- packages/mercury/examples/tree/tree.vue
+++ packages/mercury/examples/tree/tree.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="tree-box">
     <h2>yn-tree</h2>
-    <yn-tree :data="data" @pick="handlePicked"></yn-tree>
+    <div>
+      <p>default</p>
+      <yn-tree :data="data" @pick="handlePicked"></yn-tree>
+    </div>
+    <div>
+      <p>checkbox</p>
+      <yn-tree :data="data2" showCheckbox @pick="handlePicked"></yn-tree>
+    </div>
   </div>
 </template>
 <script>
@@ -10,6 +17,41 @@ export default {
   data() {
     return {
       data: [{
+        label: 'Level one 1',
+        children: [{
+          label: 'Level two 1-1',
+          children: [{
+            label: 'Level three 1-1-1'
+          }]
+        }]
+      }, {
+        label: 'Level one 2',
+        children: [{
+          label: 'Level two 2-1',
+          children: [{
+            label: 'Level three 2-1-1'
+          }]
+        }, {
+          label: 'Level two 2-2',
+          children: [{
+            label: 'Level three 2-2-1'
+          }]
+        }]
+      }, {
+        label: 'Level one 3',
+        children: [{
+          label: 'Level two 3-1',
+          children: [{
+            label: 'Level three 3-1-1'
+          }]
+        }, {
+          label: 'Level two 3-2',
+          children: [{
+            label: 'Level three 3-2-1'
+          }]
+        }]
+      }],
+      data2: [{
         label: 'Level one 1',
         children: [{
           label: 'Level two 1-1',
