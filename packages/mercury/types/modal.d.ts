15aef061431050a654426019c024d92a490cd1dc
Just be free <justbefree@126.com>
2021-09-07 09:12:17 +0800

    fix: d.ts file missing

---
+++ packages/mercury/types/modal.d.ts
@@ -0,0 +1,31 @@
+/*
+* @Author: Just be free
+* @Date:   2021-09-07 09:08:32
+* @Last Modified by:   Just be free
+* @Last Modified time: 2021-09-07 09:11:38
+* @E-mail: justbefree@126.com
+*/
+
+export type ModalOptions = {
+    message: string,
+    width?: string,
+    showCancelButton?: boolean,
+    showConfirmButton?: boolean,
+    cancelButtonText?: string,
+    cancelLoadingText?: string,
+    confirmButtonText?: string,
+    confirmLoadingText?: string,
+    loadingColor?: string,
+    closeModelOnClick?: boolean,
+    zIndex?: number,
+    showHeader?: boolean,
+    showFooter?: boolean,
+    footerTopBorder?: boolean,
+    buttonSize?: string,
+}
+export interface Modal {
+    alert(options: ModalOptions): void;
+    confirm(options: ModalOptions): void;
+}
+
+export const Modal: Modal;
