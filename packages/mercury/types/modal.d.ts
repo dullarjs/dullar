5b65663f56950590cc1dd4a352ec3f5d16b7fb25
Just be free <justbefree@126.com>
2021-09-09 12:00:27 +0800

    fix: types error

--- packages/mercury/types/modal.d.ts
+++ packages/mercury/types/modal.d.ts
@@ -2,26 +2,28 @@
 * @Author: Just be free
 * @Date:   2021-09-07 09:08:32
 * @Last Modified by:   Just be free
-* @Last Modified time: 2021-09-08 18:21:47
+* @Last Modified time: 2021-09-09 11:58:04
 * @E-mail: justbefree@126.com
 */
 
 export type ModalOptions = {
-    message: string,
-    width?: string,
-    showCancelButton?: boolean,
-    showConfirmButton?: boolean,
-    cancelButtonText?: string,
-    cancelLoadingText?: string,
-    confirmButtonText?: string,
-    confirmLoadingText?: string,
-    loadingColor?: string,
-    closeModelOnClick?: boolean,
-    zIndex?: number,
-    showHeader?: boolean,
-    showFooter?: boolean,
-    footerTopBorder?: boolean,
-    buttonSize?: string,
+    message: string;
+    width?: string;
+    showCancelButton?: boolean;
+    showConfirmButton?: boolean;
+    cancelButtonText?: string;
+    cancelLoadingText?: string;
+    confirmButtonText?: string;
+    confirmLoadingText?: string;
+    loadingColor?: string;
+    closeModelOnClick?: boolean;
+    zIndex?: number;
+    showHeader?: boolean;
+    showFooter?: boolean;
+    footerTopBorder?: boolean;
+    buttonSize?: string;
+    beforeClose?: (e: string) => void;
+    buttonClick?: (e: string, res?: any) => void;
 }
 export interface Modal {
     alert(options: ModalOptions): void;
