1f1e084b514e4c57c8cbc608f9d0e45e4a2bb045
Just be free <justbefree@126.com>
2021-09-13 19:21:19 +0800

    docs: add slide component examples

---
+++ packages/mercury/examples/slide/slide.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <h2>yn-slide</h2>
+    <ul>
+      <li>
+        <yn-slide class="one">
+          <template slot="content">一个按钮，尝试左滑呢？</template>
+          <template slot="buttons">
+            <span class="buttons" @click="handleClick('确认')">确定</span>
+          </template>
+        </yn-slide>
+      </li>
+      <li>
+        <yn-slide class="two">
+          <template slot="content">两个按钮，尝试左滑呢？</template>
+          <template slot="buttons">
+            <span class="buttons" @click="handleClick('确认')">确定</span>
+            <span class="buttons" @click="handleClick('删除')">删除</span>
+          </template>
+        </yn-slide>
+      </li>
+      <li>
+        <yn-slide class="three">
+          <template slot="content">三个按钮，尝试左滑呢？</template>
+          <template slot="buttons">
+            <span class="buttons" @click="handleClick('确认')">确定</span>
+            <span class="buttons" @click="handleClick('删除')">删除</span>
+            <span class="buttons" @click="handleClick('编辑')">编辑</span>
+          </template>
+        </yn-slide>
+      </li>
+    </ul>
+  </div>
+</template>
+<script type="text/javascript">
+export default {
+  name: "YnSliderPage",
+  data() {
+    return {
+    };
+  },
+  methods: {
+    handleClick(msg) {
+      this.Toast(msg);
+    }
+  }
+};
+</script>
+<style type="text/css" scoped="scoped">
+  ul {
+    padding: 0;
+    margin: 0;
+  }
+  li {
+    text-align: center;
+    list-style: none;
+    height: 40px;
+    line-height: 40px;
+    color: #ccc;
+    background: #ccc;
+    margin: 20px auto;
+  }
+  .one {
+    background: red;
+  }
+  .two {
+    background: yellow;
+  }
+  .three {
+    background: green;
+  }
+  .buttons {
+    background: #eee;
+    color: #fff;
+    display: inline-block;
+  }
+</style>
