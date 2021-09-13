f6c5794ea3e2f4d1236165ef3f4f0ad49066e181
Just be free <justbefree@126.com>
2021-09-13 19:20:55 +0800

    feat: add slide compnonent

---
+++ packages/mercury/src/slide/style/index.js
@@ -0,0 +1,8 @@
+/*
+* @Author: Just be free
+* @Date:   2021-09-13 15:18:56
+* @Last Modified by:   Just be free
+* @Last Modified time: 2021-09-13 15:19:14
+* @E-mail: justbefree@126.com
+*/
+import "./index.less";
