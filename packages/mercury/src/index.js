cbf095447a1f358b3c477725c94c256d44bd6a7f
liuzhx <liuzhx@yuanian.com>
2021-09-14 16:42:46 +0800

    toopTipbug修复

--- packages/mercury/src/index.js
+++ packages/mercury/src/index.js
@@ -1,4 +1,3 @@
-
 const EVENTS = {};
 import { getConfig } from "./modules/component/config";
 import { hyphenate } from "./modules/utils";
@@ -51,16 +50,61 @@ import Toast from "./toast";
 import Tooltip from "./tooltip";
 import Tree from "./tree";
 
-const components = [ActionSheet, Address, AnimationList, AnimationListItem, Badge, Button, Calendar, Category, CategoryMobile, CategorySelect, Checkbox, Counter, DatePicker, Dialog, ElasticSearch, Field, FieldGroup, Flex, FlexItem, Iconfont, Indicator, Input, Layout, Magnifier, Modal, Pagination, Picker, PickyStepper, Popup, PullRefresh, Radiobox, ShippingAddress, Skeleton, Slider, Spin, Sticky, Swipe, SwipeItem, TabItem, Table, Tabs, Toast, Tooltip, Tree];
+const components = [
+  ActionSheet,
+  Address,
+  AnimationList,
+  AnimationListItem,
+  Badge,
+  Button,
+  Calendar,
+  Category,
+  CategoryMobile,
+  CategorySelect,
+  Checkbox,
+  Counter,
+  DatePicker,
+  Dialog,
+  ElasticSearch,
+  Field,
+  FieldGroup,
+  Flex,
+  FlexItem,
+  Iconfont,
+  Indicator,
+  Input,
+  Layout,
+  Magnifier,
+  Modal,
+  Pagination,
+  Picker,
+  PickyStepper,
+  Popup,
+  PullRefresh,
+  Radiobox,
+  ShippingAddress,
+  Skeleton,
+  Slider,
+  Spin,
+  Sticky,
+  Swipe,
+  SwipeItem,
+  TabItem,
+  Table,
+  Tabs,
+  Toast,
+  Tooltip,
+  Tree,
+];
 const install = (Vue) => {
   if (install.installed) return;
-  components.map(component => {
+  components.map((component) => {
     if (component.name && typeof component !== "function") {
       const eventName = hyphenate(component.name);
       if (EVENTS[eventName] && typeof EVENTS[eventName] === "function") {
-         Vue.component(component.name, EVENTS[eventName]());
+        Vue.component(component.name, EVENTS[eventName]());
       } else {
-         Vue.component(component.name, component);
+        Vue.component(component.name, component);
       }
     } else if (component.install) {
       Vue.use(component);
@@ -69,29 +113,30 @@ const install = (Vue) => {
   Vue.prototype.Toast = Toast;
   Vue.prototype.Dialog = Dialog;
   Vue.prototype.Indicator = Indicator;
+  Vue.prototype.Modal = Modal;
 };
 if (typeof window !== "undefined" && window.Vue) {
   install(window.Vue);
 }
 const config = (options = {}) => {
-  Object.keys(options).forEach(componentName => {
-   const ca = hyphenate(componentName);
-   components.forEach((component) => {
-     if (ca === component.name) {
-       if (component.callback && (typeof component.callback === "function")) {
-         EVENTS[ca] = () => {
-           return component.callback(options[componentName]);
-         }
-       }
-     }
-   });
+  Object.keys(options).forEach((componentName) => {
+    const ca = hyphenate(componentName);
+    components.forEach((component) => {
+      if (ca === component.name) {
+        if (component.callback && typeof component.callback === "function") {
+          EVENTS[ca] = () => {
+            return component.callback(options[componentName]);
+          };
+        }
+      }
+    });
   });
 };
 export { install, version, config };
 export default {
   install,
   version,
-  config
+  config,
 };
 export { ActionSheet as YnActionSheet };
 export { Address as YnAddress };
