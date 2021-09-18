a0e54a9f6f9ba6d006f6351cb9810ed60dd9ce92
Just be free <justbefree@126.com>
2021-09-18 21:00:17 +0800

    feat: skeleton support card display mode

--- packages/mercury/src/skeleton/skeleton.js
+++ packages/mercury/src/skeleton/skeleton.js
@@ -2,7 +2,7 @@
  * @Author: Just be free
  * @Date:   2020-11-03 11:12:31
  * @Last Modified by:   Just be free
- * @Last Modified time: 2020-11-03 18:43:24
+ * @Last Modified time: 2021-09-18 20:58:39
  * @E-mail: justbefree@126.com
  */
 import { defineComponent } from "../modules/component";
@@ -13,6 +13,10 @@ export default defineComponent({
   name: "Skeleton",
   props: {
     margin: String,
+    type: {
+      type: String,
+      default: ""
+    },
     rows: {
       type: [String, Number],
       default: 2,
@@ -37,6 +41,10 @@ export default defineComponent({
       "div",
       { class: ["yn-skeleton"], style: { margin: this.margin } },
       [
+        h("div", {
+          class: ["animated-background", "background-card"],
+          directives: [{ name: "show", value: this.type === "card" }]
+        }, []),
         h(
           "div",
           {
