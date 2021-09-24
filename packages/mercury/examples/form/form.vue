824137611f12cdb93c2742e6893d08dfe6925d80
tongh <tongh@yuanian.com>
2021-09-24 13:50:45 +0800

    form表单分支

---
+++ packages/mercury/examples/form/form.vue
@@ -0,0 +1,34 @@
+<template>
+  <form class="form">
+    <slot></slot>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "form",
+  props: {
+    rule: {
+      type: Object,
+      default() {},
+    },
+    model: {
+      type: Object,
+    },
+    "label-width": {
+      type: String,
+    },
+    inline: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  provide() {
+    return {
+      formWrap: this,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ packages/mercury/examples/form/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: tongh
+ * @Date:   2020-09-17 15:51:17
+ * @Last Modified by:   tongh
+ * @Last Modified time: 2020-09-17  17:56:47
+ */
+
+import { install } from "../modules/component";
+import YnForm from "./form.vue";
+export default install(YnForm);
