ae5d9bcc3da132ac0ddd092f9fb905417042fb8e
Just be free <justbefree@126.com>
2021-09-28 14:22:45 +0800

    docs: update toast examples

--- packages/mercury/examples/toast/toast.vue
+++ packages/mercury/examples/toast/toast.vue
@@ -4,16 +4,21 @@
     <ul>
       <li @click="toast('middle')">toast middle</li>
       <li @click="toast('bottom')">toast bottom</li>
+      <li @click="functional">functional toast</li>
     </ul>
   </div>
 </template>
 <script type="text/javascript">
+import { YnToast } from "@";
 export default {
   name: "YnToastPage",
   data() {
     return {};
   },
   methods: {
+    functional() {
+      YnToast("this is functional toast");
+    },
     toast(position) {
       this.Toast({ message: `hello , ${position}`, position });
     }
