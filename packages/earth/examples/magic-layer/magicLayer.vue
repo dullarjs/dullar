967d5834456ec3d25266c6c570b8339569cab371
Just be free <justbefree@126.com>
2021-12-08 15:52:51 +0800

    docs: add magic-layer component example

---
+++ packages/earth/examples/magic-layer/magicLayer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <!-- <h2>yn-magic-layer</h2> -->
+    <yn-magic-layer>
+      <div class="inner" slot="inner">1</div>
+      <div slot="outside">2</div>
+    </yn-magic-layer>
+  </div>
+</template>
+<script type="text/javascript">
+  export default {
+    name: "YnMagicLayerPage",
+    mounted() {
+    }
+  };
+</script>
+<style type="text/css" scoped="scoped">
+  .container {
+    height: 100%;
+  }
+  .inner {
+    height: 200px;
+  }
+</style>
