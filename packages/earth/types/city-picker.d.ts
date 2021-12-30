57b38f793b9a5f90c2b3c49d10327200f7f9aa33
Strong <justbefree@126.com>
2021-12-30 17:22:02 +0800

    feat: modify d.ts file

--- packages/earth/types/city-picker.d.ts
+++ packages/earth/types/city-picker.d.ts
@@ -2,11 +2,12 @@
 * @Author: Just be free
 * @Date:   2020-12-17 18:15:02
 * @Last Modified by:   Just be free
-* @Last Modified time: 2020-12-17 18:16:03
+* @Last Modified time: 2021-12-30 17:21:32
 * @E-mail: justbefree@126.com
 */
 
 import { YnComponents } from "./component";
 export class YnCityPicker extends YnComponents {
   close(): void;
+  clearSearchKeywords(): void;
 }
