60a4aa0349b402a798c4e0c8412ffd743bff8c47
Strong <justbefree@126.com>
2021-12-31 18:05:17 +0800

    fix: types file

--- packages/earth/types/dropdown-menu-item.d.ts
+++ packages/earth/types/dropdown-menu-item.d.ts
@@ -2,7 +2,7 @@
 * @Author: Just be free
 * @Date:   2020-12-17 16:41:13
 * @Last Modified by:   Just be free
-* @Last Modified time: 2021-12-31 17:53:49
+* @Last Modified time: 2021-12-31 18:04:54
 * @E-mail: justbefree@126.com
 */
 
@@ -14,6 +14,6 @@ export class YnDropdownMenuItem extends YnComponents {
   closeTab(): void;
   toggle(args: ToggleOptions): void;
   check(status: boolean, options?: ToggleOptions): void;
-  checkMapOption(index: number, options: ToggleOptions): void;
+  checkMapOption(index: number, options?: ToggleOptions): void;
   setIsFiltered(status: boolean): void;
 }
