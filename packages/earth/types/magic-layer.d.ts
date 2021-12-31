c208835b7b11e123c27345423c8200763e3aac55
Strong <justbefree@126.com>
2021-12-31 17:41:36 +0800

    feat: add .d.ts files

---
+++ packages/earth/types/magic-layer.d.ts
@@ -0,0 +1,12 @@
+/*
+* @Author: Just be free
+* @Date:   2021-12-31 17:38:55
+* @Last Modified by:   Just be free
+* @Last Modified time: 2021-12-31 17:40:37
+* @E-mail: justbefree@126.com
+*/
+
+import { YnComponents } from "./component";
+export class YnMagicLayer extends YnComponents {
+  fold(): void;
+}
---
+++ packages/earth/types/textarea.d.ts
@@ -0,0 +1,10 @@
+/*
+* @Author: Just be free
+* @Date:   2021-12-31 17:38:37
+* @Last Modified by:   Just be free
+* @Last Modified time: 2021-12-31 17:39:38
+* @E-mail: justbefree@126.com
+*/
+
+import { YnComponents } from "./component";
+export class YnTextarea extends YnComponents {}
