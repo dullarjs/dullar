cda71c0a7a280ef0b489d1e119d059bf68656fbe
Strong <justbefree@126.com>
2022-01-04 09:42:42 +0800

    docs: toast下新增短信发送

--- packages/earth/examples/toast/toast.vue
+++ packages/earth/examples/toast/toast.vue
@@ -6,6 +6,9 @@
       <li @click="toast('bottom')">toast bottom</li>
       <li @click="toast('top')">toast top</li>
       <li @click="toast1">toast default</li>
+      <li>
+        <a ref="sms" @click="sendSMS">发送短信</a>
+      </li>
     </ul>
   </div>
 </template>
@@ -16,6 +19,17 @@ export default {
     return {};
   },
   methods: {
+    isIOS() {
+      const ua = window.navigator.userAgent;
+      return !!ua.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // ios终端;
+    },
+    sendSMS() {
+      if (this.isIOS()) {
+        this.$refs.sms.setAttribute("href", "sms:10086&body=888");
+      } else {
+        this.$refs.sms.setAttribute("href", "sms:10086?body=888");
+      }
+    },
     toast(position) {
       this.Toast({ message: `hello , ${position}`, position });
     },
