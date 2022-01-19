951783c0fc2f05e29cb93686a6bb2f27b2384041
wangshz <wangshz@yuanian.com>
2022-01-19 16:07:26 +0800

    /src/index.js 改成/src/index.ts

--- packages/mars/src/index.ts
+++ packages/mars/src/index.ts
@@ -1,12 +1,14 @@
+import { VueConstructor } from "vue/types/umd";
 import Dialog from "./components/dialog";
 import Popover from "./components/popover";
+import { AnyObject } from "./types";
 
-export const components = {
+export const components: AnyObject = {
   Dialog,
   Popover
 }
 
-const install = (Vue) => {
+const install = (Vue: VueConstructor) => {
   Object.keys(components).map(compName => {
     const comp = components[compName];
     Vue.component(compName, comp);
