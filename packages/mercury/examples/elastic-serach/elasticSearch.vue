778036fde383c2616d5494d38f25edd9d856c828
liuzhx <liuzhx@yuanian.com>
2022-01-20 13:32:51 +0800

    修改文案

--- packages/mercury/examples/elastic-serach/elasticSearch.vue
+++ packages/mercury/examples/elastic-serach/elasticSearch.vue
@@ -4,7 +4,7 @@
     <yn-elastic-search closeWhenSearch v-model="keywords" :history="getHistory()" :delete="deleteHistory()" v-on:fireSearch="hanleFireSearch" placeholder="请输入要搜索的商品"></yn-elastic-search>
     <h2>yn-elastic-search label 插槽</h2>
     <yn-elastic-search closeWhenSearch v-model="keywords" :history="getHistory()" :delete="deleteHistory()" v-on:fireSearch="hanleFireSearch" placeholder="请输入要搜索的商品">
-      <span slot="label" class="label">插槽11</span>
+      <span slot="label" class="label">插槽</span>
     </yn-elastic-search>
   </div>
 </template>
