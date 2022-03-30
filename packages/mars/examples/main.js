ce96807ab617007e480663a3ce46af4ff4cf1d68
Strong <justbefree@126.com>
2022-03-30 14:20:33 +0800

    chore: module federation supported

--- packages/mars/examples/main.js
+++ packages/mars/examples/main.js
@@ -2,28 +2,8 @@
  * @Author: Just be free
  * @Date:   2020-01-20 17:15:01
  * @Last Modified by:   Just be free
- * @Last Modified time: 2020-09-11 14:22:44
+ * @Last Modified time: 2022-03-30 13:51:21
  * @E-mail: justbefree@126.com
  */
 
-import Vue from "vue";
-import App from "./App";
-import router from "./router";
-
-import "normalize.css";
-import "@/theme/global.scss";
-import svgs from "./iconfont/svgs";
-
-import Mars from "../src/index";
-Mars.config({ YnIconfont: svgs });
-Vue.use(Mars);
-
-// YuiComponent.config({ iconfont: "../" });
-if (process.env.NODE_ENV === "development") {
-  // const VConsole = require("vconsole");
-  // new VConsole();
-}
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount("#example");
+import("./bootstrap");
--- packages/mars/vue.config.js
+++ packages/mars/vue.config.js
@@ -1,9 +1,46 @@
+const { defineConfig } = require('@vue/cli-service');
+const ModuleFederationPlugin =
+  require("webpack").container.ModuleFederationPlugin;
 const publicPath = process.env.NODE_ENV === 'production' ? "/mars/" : "/local/";
-module.exports = {
+module.exports = defineConfig({
   publicPath,
   outputDir: "docs",
   css: {
     extract: false
+  },
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.(svg)(\?.*)?$/,
+          type: 'asset',
+          parser: {
+             dataUrlCondition: {
+               maxSize: 20 * 1024 // 20kb
+             }
+          },
+          use: 'svgo-loader'
+        }
+      ]
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+            // runtime: "dullarjs-runtime",
+        runtime: false,
+        name: "dullarjs",
+        // library: { type: "umd", name: "flight" },
+        // library: { type: "umd" },
+        filename: "remoteEntry.js",
+        exposes: {
+          "./mars": "./lib/@dullar/mars.umd.min"
+        },
+        remotes: {
+          // hotel: "hotel@http://localhost:8082/remoteEntry.js",
+          // train: "train@http://localhost:8083/remoteEntry.js",
+        },
+        shared: ["vue", "vue-router", "core-js"]
+      })
+    ]
   },
   pages: {
     index: {
@@ -13,4 +50,4 @@ module.exports = {
       title: "Examples for one-ui components"
     }
   }
-}
+});
